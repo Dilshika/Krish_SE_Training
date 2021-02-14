4cd99bcb7bb6e93ae08861852dd6b7f67f5a3a1d
Dilshika <30935988+Dilshika@users.noreply.github.com>
2021-02-14 11:41:49 +0530

    Update Application.java

--- DesignPatterns/src/main/java/com/dil/designpatterns/chainofresponsibility/Application.java
+++ DesignPatterns/src/main/java/com/dil/designpatterns/chainofresponsibility/Application.java
@@ -1,4 +1,4 @@
-package main.java.com.virtusa.designpatterns.chainofresponsibility;
+package main.java.com.dil.designpatterns.chainofresponsibility;
 
 public class Application {
     public static void main(String[] args) {
