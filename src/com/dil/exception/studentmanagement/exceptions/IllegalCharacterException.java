17b3810c83f349c76532a5317f200f411f9fcc9c
Dilshika <30935988+Dilshika@users.noreply.github.com>
2021-02-14 11:56:12 +0530

    Update IllegalCharacterException.java

--- src/com/dil/exception/studentmanagement/exceptions/IllegalCharacterException.java
+++ src/com/dil/exception/studentmanagement/exceptions/IllegalCharacterException.java
@@ -1,4 +1,4 @@
-package com.virtusa.exception.studentmanagement.exceptions;
+package com.dil.exception.studentmanagement.exceptions;
 
 public class IllegalCharacterException extends Exception{
     private static final long serialVersionUID=1L;
